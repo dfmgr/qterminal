de8be42ea7c1a0c2300f3421dc19da879ecdb609
Jason <jason@casjay.net>
2021-04-08 12:15:35 -0400

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -51,10 +51,10 @@ scripts_check
 # Defaults
 APPNAME="${APPNAME:-qterminal}"
 APPDIR="${APPDIR:-$HOME/.config/$APPNAME}"
-INSTDIR="${INSTDIR}"
-REPO="${DFMGRREPO:-https://github.com/dfmgr/$APPNAME}"
-REPORAW="${REPORAW:-$REPO/raw}"
-APPVERSION="$(__appversion "$REPORAW/master/version.txt")"
+INSTDIR="${INSTDIR:-$HOME/.local/share/CasjaysDev/dfmgr/$APPNAME}"
+REPO="${REPO:-https://github.com/dfmgr/$APPNAME}"
+REPORAW="${REPORAW:-$REPO/$GIT_REPO_BRANCH}"
+APPVERSION="$(__appversion "$REPORAW/version.txt")"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Setup plugins
 PLUGNAMES=""
@@ -122,7 +122,7 @@ if __am_i_online; then
   if [ -d "$INSTDIR/.git" ]; then
     execute "git_update $INSTDIR" "Updating $APPNAME configurations"
   else
-    execute "git_clone $REPO/$APPNAME $INSTDIR" "Installing $APPNAME configurations"
+    execute "git_clone $REPO $INSTDIR" "Installing $APPNAME configurations"
   fi
   # exit on fail
   failexitcode $? "Git has failed"
